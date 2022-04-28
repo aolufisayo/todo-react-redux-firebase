93605debb8d953a49d2e348bea09a5a985e6e419
aolufisayo <phissycodes@hotmail.com>
2022-04-28 10:13:42 +0100

    add todo to state

--- src/features/todos/AddTodo.js
+++ src/features/todos/AddTodo.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "./todoSlice";
 
 export const AddTodo = () => {
+  const dispatch = useDispatch();
+  const [todo, setTodo] = useState({ text: "" });
+
+  const clearInputField = (event) => {
+    event.target.value = "";
+  };
   return (
     <div>
       <div className="pb-2">
@@ -8,10 +16,16 @@ export const AddTodo = () => {
           <div className="card-body">
             <div className="d-flex flex-row align-items-center">
               <input
+                onChange={(event) => {
+                  setTodo({
+                    text: event.target.value,
+                  });
+                }}
                 type="text"
                 className="form-control form-control-lg"
                 id="exampleFormControlInput1"
                 placeholder="Add new Todo..."
+                onBlur={(event) => clearInputField(event)}
               ></input>
               <div>
                 <button type="button" className="btn btn-primary">
